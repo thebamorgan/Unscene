89f12e32c539088782987fba40a9914ec0a6d32c
ultra <ultra@Zander-Surface>
2022-10-19 16:34:21 -0500

    Added draft of Movie class

---
+++ Unscene/src/home/Movie.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package home;
+
+/**
+ *
+ * @author ultra
+ */
+public class Movie {
+    String Title; //Title - Title
+    String RDate; // RDate - Release Date
+    int RunT; // RunT - Runtime in seconds
+    String Genre; // Genre - Genre of movie || Think about making it possible to have multiple genres
+    String Desc; // Desc - Description of movie
+    String Cast; // Cast - A cast member in the movie || Definitely figure out how to make Cast into an array to store multiple cast members
+    String Rating; // Rating - Content maturity rating of the movie || Look into how movie metadata stores rating. May need to change type
+    boolean Watched; // Watched - Has the user seen this movie? 0 = No, 1 = Yes
+    boolean Interest; // Interest - Is the user interested in this movie? 0 = No, 1 = Yes
+}
